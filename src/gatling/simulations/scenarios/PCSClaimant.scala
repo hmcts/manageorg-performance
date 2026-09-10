7c067adda50ca24bbef1c07ca81996d827d2cb09
Dean Cullen <Dean.Cullen@hmcts.net>
2026-09-10 11:29:14 +0100

    Tidy claimant flow

--- src/gatling/simulations/scenarios/PCSClaimant.scala
+++ src/gatling/simulations/scenarios/PCSClaimant.scala
@@ -5,6 +5,7 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scenarios.api.payments
 import utilities._
+import utils.Environment
 
 object PCSClaimant {
 
@@ -24,42 +25,87 @@ object PCSClaimant {
 				.set("housingEmail", session("email").as[String])
 				.set("housingPassword", session("password").as[String]))
 
+		/*=============================================
+		Create PCS Case as claimant
+		===============================================*/
+
 		.exec(CcdHelper.createCase("#{housingEmail}", "#{housingPassword}", CcdCaseTypes.PCS_PCS, "createPossessionClaim", "bodies/pcsBodies/PCSCreateCase.json"))
+		.pause(Environment.thinkTime)
+
 		.feed(feedPCSCWUserData)
 			.exec(session => session
 				.set("cwEmail", session("email").as[String])
 				.set("cwPassword", session("password").as[String]))
-	
+
+		/*=============================================
+		Upload Tenancy Document
+		===============================================*/
+
 		.exec(CcdHelper.uploadDocumentToCdam("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
 			jsonPath("$.documents[0]._links.self.href").saveAs("TenancyDocumentURL"),
 			jsonPath("$.documents[0].hashToken").saveAs("TenancyDocumentHash")
 		)))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Upload Notice Document
+		===============================================*/
+
 		.exec(CcdHelper.uploadDocumentToCdam("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
 			jsonPath("$.documents[0]._links.self.href").saveAs("NoticeDocumentURL"),
 			jsonPath("$.documents[0].hashToken").saveAs("NoticeDocumentHash")
 		)))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Upload Rent Arrears Document
+		===============================================*/
+
 		.exec(CcdHelper.uploadDocumentToCdam("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
 			jsonPath("$.documents[0]._links.self.href").saveAs("RentArrearsDocumentURL"),
 			jsonPath("$.documents[0].hashToken").saveAs("RentArrearsDocumentHash")
 		)))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Upload Rent Statement Document
+		===============================================*/
+
 		.exec(CcdHelper.uploadDocumentToCdam("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
 			jsonPath("$.documents[0]._links.self.href").saveAs("RentStatementDocumentURL"),
 			jsonPath("$.documents[0].hashToken").saveAs("RentStatementDocumentHash")
 		)))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Upload Rent Statement Document
+		===============================================*/
+
 		.exec(CcdHelper.uploadDocumentToCdam("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
 			jsonPath("$.documents[0]._links.self.href").saveAs("TenancyAgreementDocumentURL"),
 			jsonPath("$.documents[0].hashToken").saveAs("TenancyAgreementDocumentHash")
 		)))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Submit Claim
+		===============================================*/
+
 		.exec(CcdHelper.addCaseEvent("#{housingEmail}", "#{housingPassword}", CcdCaseTypes.PCS_PCS, "#{caseId}", "resumePossessionClaim", "bodies/pcsBodies/PCSSubmitClaim.json"))
+		.pause(Environment.thinkTime)
+
+		/*=============================================
+		Pay fee for claim
+		===============================================*/
 		.exec(payments.AddPCSPayment)
+		.pause(Environment.thinkTime)
+
 		.exec(_.set("pastDate", DateUtils.getDatePast("yyyy-MM-dd", days = 10)))
+
+		/*=============================================
+		Add Case Review Date case event
+		===============================================*/
 		.exec(CcdHelper.addCaseEvent("#{cwEmail}", "#{cwPassword}", CcdCaseTypes.PCS_PCS, "#{caseId}", "addCaseReviewDate", "bodies/pcsBodies/PCSAddReviewDate.json"))
-	}
-		//.feed(feedPCSCWUserData)
-//		.exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", CcdCaseTypes.PCS_PCS.copy(microservice = "pcs_api"), "1MB.pdf", additionalChecks = Seq(
-//			jsonPath("$.documents[0]._links.self.href").saveAs("GADocumentURL"),
-//			jsonPath("$.documents[0].hashToken").saveAs("GADocumentHash")
-//		)))
-//		.exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.PCS_PCS, "#{caseId}", "enterGenApp", "pcsBodies/PCSEnterGeneralApplication.json"))
-				//.exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.PCS_PCS, "#{caseId}", "changeCaseState", "bodies/pcsBodies/PCSChangeState.json"))
-	}
+		.pause(Environment.thinkTime)
+	}	
+}
